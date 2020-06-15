8e159d60c23019dd5db6d150723f7880b05f135d
george <george@esigma.com.br>
2020-06-15 20:40:20 -0300

    Exercicio 1805

---
+++ Exercicio1805/Exercicio1805/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1805
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] entrada = Console.ReadLine().Split(' ');
+             
+            int a1 = int.Parse(entrada[0]);
+            int an = int.Parse(entrada[1]);
+            double n = an - a1 + 1;
+            double soma = (a1 + an) * n / 2.0;
+            Console.WriteLine($"{soma:0}");
+            Console.ReadKey();
+        }
+    }
+}
